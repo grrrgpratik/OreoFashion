832ce8f8c8add27a82e42c7d2ae5b56aaca94303
grgpratik <gurung.pratik04@gmail.com>
2020-08-22 00:11:24 +0545

    category, prodcut, order model and database connectivity added

---
+++ projbackend/models/order.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Schema;
+
+const ProductCartSchema = new mongoose.Schema({
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: {
+      type: String,
+    },
+    updated: {
+      type: Date,
+    },
+    user: { type: ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
+
+module.exports = { Order, ProductCart };
---
+++ projbackend/models/product.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Schema;
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    stock: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: false,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.exports("Product", productSchema);
